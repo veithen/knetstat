a7ac774b7b2916e428acbef022d17291db934e1a
Andreas Veithen <andreas.veithen@gmail.com>
2013-12-18 16:50:58 +0100

    Added support for IPv6 (TCP)

--- knetstat.c
+++ knetstat.c
@@ -25,18 +25,19 @@ static const char *const tcp_state_names[] = {
 		"CLOSING",
 };
 
-static int tcp4_seq_show(struct seq_file *seq, void *v) {
+static int tcp_seq_show(struct seq_file *seq, void *v) {
 	if (v == SEQ_START_TOKEN) {
 		seq_printf(seq, "Proto Recv-Q Send-Q Local Address           Foreign Address         State       Options\n");
 	} else {
 		struct tcp_iter_state *st = seq->private;
+		sa_family_t	family = st->family;
 		int pos;
 		int len;
 
 		int rx_queue;
 		int tx_queue;
-		__be32 dest;
-		__be32 src;
+		const void *dest;
+		const void *src;
 		__u16 destp;
 		__u16 srcp;
 		int state;
@@ -52,15 +53,21 @@ static int tcp4_seq_show(struct seq_file *seq, void *v) {
 				tp = tcp_sk(sk);
 				inet = inet_sk(sk);
 
-				// See get_tcp4_sock in tcp_ipv4.c
+				// See get_tcp4_sock in tcp_ipv4.c and get_tcp6_sock in tcp_ipv6.c
 				if (sk->sk_state == TCP_LISTEN) {
 					rx_queue = sk->sk_ack_backlog;
 				} else {
 					rx_queue = max_t(int, tp->rcv_nxt - tp->copied_seq, 0);
 				}
 				tx_queue = tp->write_seq - tp->snd_una;
-				dest = inet->inet_daddr;
-				src = inet->inet_rcv_saddr;
+				if (family == AF_INET6) {
+					const struct ipv6_pinfo *np = inet6_sk(sk);
+					dest = &np->daddr;
+					src = &np->rcv_saddr;
+				} else {
+					dest = &inet->inet_daddr;
+					src = &inet->inet_rcv_saddr;
+				}
 				destp = ntohs(inet->inet_dport);
 				srcp = ntohs(inet->inet_sport);
 				state = sk->sk_state;
@@ -70,12 +77,18 @@ static int tcp4_seq_show(struct seq_file *seq, void *v) {
 			case TCP_SEQ_STATE_OPENREQ: {
 				const struct inet_request_sock *ireq = inet_rsk(v);
 
-				// See get_openreq4 in tcp_ipv4.c
+				// See get_openreq4 in tcp_ipv4.c and get_openreq6 in tcp_ipv6.c
 				rx_queue = 0;
 				tx_queue = 0;
-				src = ireq->loc_addr;
+				if (family == AF_INET6) {
+					const struct inet6_request_sock *ireq6 = inet6_rsk(v);
+					src = &ireq6->loc_addr;
+					dest = &ireq6->rmt_addr;
+				} else {
+					src = &ireq->loc_addr;
+					dest = &ireq->rmt_addr;
+				}
 				srcp = ntohs(inet_sk(st->syn_wait_sk)->inet_sport);
-				dest = ireq->rmt_addr;
 				destp = ntohs(ireq->rmt_port);
 				state = TCP_SYN_RECV;
 				sk = NULL;
@@ -85,11 +98,17 @@ static int tcp4_seq_show(struct seq_file *seq, void *v) {
 			case TCP_SEQ_STATE_TIME_WAIT: {
 				const struct inet_timewait_sock *tw = v;
 
-				// See get_timewait4_sock in tcp_ipv4.c
+				// See get_timewait4_sock in tcp_ipv4.c and get_timewait6_sock in tcp_ipv6.c
 				rx_queue = 0;
 				tx_queue = 0;
-				dest = tw->tw_daddr;
-				src = tw->tw_rcv_saddr;
+				if (family == AF_INET6) {
+					const struct inet6_timewait_sock *tw6 = inet6_twsk((struct sock *)tw);
+					dest = &tw6->tw_v6_daddr;
+					src  = &tw6->tw_v6_rcv_saddr;
+				} else {
+					dest = &tw->tw_daddr;
+					src = &tw->tw_rcv_saddr;
+				}
 				destp = ntohs(tw->tw_dport);
 				srcp = ntohs(tw->tw_sport);
 				state = tw->tw_substate;
@@ -105,16 +124,16 @@ static int tcp4_seq_show(struct seq_file *seq, void *v) {
 			state = 0;
 		}
 
-		seq_printf(seq, "%-5s %6d %6d %pI4:%d%n", "tcp", rx_queue, tx_queue, &src, srcp, &pos);
-		seq_printf(seq, "%*s", 44-pos, "");
-		seq_printf(seq, "%pI4:%n", &dest, &len);
-		pos = 44+len;
+		seq_printf(seq, "%-5s %6d %6d %n", family == AF_INET6 ? "tcp6" : "tcp", rx_queue, tx_queue, &pos);
+		seq_printf(seq, family == AF_INET6 ? "%pI6c%n" : "%pI4%n", src, &len); pos += len;
+		seq_printf(seq, ":%d%n", srcp, &len); pos += len;
+		seq_printf(seq, "%*s%n", 44-pos, "", &len); pos += len;
+		seq_printf(seq, family == AF_INET6 ? "%pI6c%n" : "%pI4%n", dest, &len); pos += len;
 		if (destp == 0) {
-			seq_printf(seq, "%s%n", "*", &len);
+			seq_printf(seq, ":*"); pos += 2;
 		} else {
-			seq_printf(seq, "%d%n", destp, &len);
+			seq_printf(seq, ":%d%n", destp, &len); pos += len;
 		}
-		pos += len;
 		seq_printf(seq, "%*s%-12s", 68-pos, "", tcp_state_names[state]);
 		if (sk != NULL) {
 			seq_printf(seq, "SO_REUSEADDR=%d,SO_KEEPALIVE=%d", sk->sk_reuse, sock_flag(sk, SOCK_KEEPOPEN));
@@ -137,16 +156,37 @@ static struct tcp_seq_afinfo tcp4_seq_afinfo = {
 		.family = AF_INET,
 		.seq_fops = &tcp_afinfo_seq_fops,
 		.seq_ops = {
-				.show = tcp4_seq_show,
+				.show = tcp_seq_show,
+		},
+};
+
+static struct tcp_seq_afinfo tcp6_seq_afinfo = {
+		.name = "tcp6stat",
+		.family = AF_INET6,
+		.seq_fops = &tcp_afinfo_seq_fops,
+		.seq_ops = {
+				.show = tcp_seq_show,
 		},
 };
 
 static int __net_init knetstat_net_init(struct net *net) {
-	return tcp_proc_register(net, &tcp4_seq_afinfo);
+	int ret;
+
+	ret = tcp_proc_register(net, &tcp4_seq_afinfo);
+	if (ret < 0) {
+		return ret;
+	}
+
+	ret = tcp_proc_register(net, &tcp6_seq_afinfo);
+	if (ret < 0) {
+		tcp_proc_unregister(net, &tcp4_seq_afinfo);
+	}
+	return ret;
 }
 
 static void __net_exit knetstat_net_exit(struct net *net) {
 	tcp_proc_unregister(net, &tcp4_seq_afinfo);
+	tcp_proc_unregister(net, &tcp6_seq_afinfo);
 }
 
 static struct pernet_operations knetstat_net_ops = { .init = knetstat_net_init,
