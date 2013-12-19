e0c8a1e96174762f0228141ea6dddb2035463152
Andreas Veithen <andreas.veithen@gmail.com>
2013-12-19 10:21:09 +0100

    Complete module metadata and add license files/headers

--- knetstat.c
+++ knetstat.c
@@ -1,3 +1,22 @@
+/**
+ * knetstat
+ * Copyright (C) 2013  Andreas Veithen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -210,3 +229,5 @@ module_init(knetstat_init)
 module_exit(knetstat_exit)
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Andreas Veithen <andreas.veithen@gmail.com>");
+MODULE_DESCRIPTION("Support for /proc/net/tcpstat and /proc/net/tcp6stat");
